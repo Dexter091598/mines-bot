6c940a64aed6f38d08784a4a5d1ed2681e632f2d
Armageddon <lunyx1337@gmail.com>
2020-06-04 13:15:44 +1000

    logger, smoth movement, fx hasher, dbscan for sectors

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
-extern crate autopilot;
-extern crate image;
 #[macro_use]
 extern crate itertools;
 #[macro_use]
 extern crate clap;
 #[macro_use]
 extern crate num_derive;
+#[macro_use]
+extern crate log;
 
 use autopilot::{
 	bitmap::{capture_screen, capture_screen_portion, Bitmap},
 	geometry::{Point, Rect, Size},
-	mouse::{click, move_to, Button},
+	mouse::{click, move_to, smooth_move, Button},
 };
 use image::open;
 use itertools::Itertools;
 use ndarray::prelude::*;
 use num_traits::cast::{FromPrimitive, ToPrimitive};
-use rand::prelude::{thread_rng, IteratorRandom};
+use rand::prelude::*;
 use rayon::prelude::*;
-use std::{
-	cmp::min,
-	collections::{HashMap, HashSet},
-	thread::sleep,
-	time::Duration,
-};
+use rustc_hash::{FxHashMap, FxHashSet, FxHasher};
+use std::{cmp::min, collections::HashSet, hash::BuildHasherDefault, thread::sleep, time::Duration};
 
 type Pos = (usize, usize);
 
@@ -31,10 +27,10 @@ type Pos = (usize, usize);
 struct Detector {
 	pos: Pos,
 	mines_left: usize,
-	unexplored: HashSet<Pos>,
+	unexplored: FxHashSet<Pos>,
 }
 impl Detector {
-	fn new(pos: Pos, mines_left: usize, unexplored: HashSet<Pos>) -> Self {
+	fn new(pos: Pos, mines_left: usize, unexplored: FxHashSet<Pos>) -> Self {
 		Self {
 			pos,
 			mines_left,
@@ -87,7 +83,9 @@ impl PartialEq<u8> for Square {
 }
 */
 
+#[allow(clippy::cognitive_complexity)]
 fn main() {
+	pretty_env_logger::init();
 	let matches = clap_app!((
 		crate_name!()
 			.split('-')
@@ -118,28 +116,46 @@ fn main() {
 			value_name("folder_name")
 			"Sets custom tiles directory."
 		)
+		(@arg smooth_move: -s --smooth
+			+takes_value
+			value_name("duration")
+			"Smooth mouse movement."
+		)
 	)
 	.get_matches();
 
-	let cooldown: u64 = match matches.value_of("cooldown") {
-		Some(val) => val.parse().unwrap(),
-		None => 0,
+	let (smooth, smooth_duration) = {
+		if matches.is_present("smooth_move") {
+			(
+				true,
+				matches
+					.value_of("smooth_move")
+					.and_then(|val| val.parse::<f64>().ok()),
+			)
+		} else {
+			(false, None)
+		}
 	};
+
+	let cooldown = matches
+		.value_of("cooldown")
+		.map_or(0, |val| val.parse::<u64>().unwrap());
 	let no_flags = matches.is_present("no_flags");
-	let tiles_dir = format!(
-		"tiles/{}",
-		(match matches.value_of("tiles") {
-			Some(val) => val.parse().unwrap(),
-			None => "default".to_string(),
-		})
-	);
+	let tiles_dir = format!("tiles/{}", matches.value_of("tiles").unwrap_or("default"));
 
 	// Pre-init
 	let mut rng = thread_rng();
 	let unexplored_tile = Bitmap::new(open(format!("{}/9.png", tiles_dir)).unwrap(), None);
+
 	let tiles = (0..=8)
 		.map(|n| Bitmap::new(open(format!("{}/{}.png", tiles_dir, n)).unwrap(), None))
 		.collect::<Vec<Bitmap>>();
+
+	/*let point = Point::new(9.0, 7.0);
+	let colors = (0..=8)
+		.map(|n| Bitmap::new(open(format!("{}/{}.png", tiles_dir, n)).unwrap(), None).get_pixel(point))
+		.collect::<Vec<Rgba<u8>>>();*/
+
 	let tile_size = unexplored_tile.size;
 	let full_tile = tile_size.width;
 	let half_tile = full_tile / 2.0;
@@ -147,9 +163,9 @@ fn main() {
 	let smiley_lose = Bitmap::new(open(format!("{}/smiley_lose.png", tiles_dir)).unwrap(), None);
 
 	// Init
-	println!("Capturing screen...");
+	debug!("Capturing screen...");
 	let screen = capture_screen().unwrap();
-	println!("Searching for unexplored tiles...");
+	debug!("Searching for unexplored tiles...");
 	let all_unexplored = screen.find_every_bitmap(&unexplored_tile, None, None, None);
 	if all_unexplored.is_empty() {
 		panic!("Can't find game on screen");
@@ -166,7 +182,7 @@ fn main() {
 		smiley_win.size,
 	);
 
-	println!("Grouping...");
+	debug!("Grouping...");
 	let grouped = &all_unexplored
 		.into_iter()
 		.group_by(|p| p.x)
@@ -174,26 +190,25 @@ fn main() {
 		.map(|g| (g.0, g.1.collect::<Vec<_>>()))
 		.collect::<Vec<_>>();
 
-	println!("Creating coordinates grid...");
-	let x_size = grouped.len();
-	let y_size = grouped[0].1.len();
-	let shape = (x_size, y_size);
+	debug!("Creating coordinates grid...");
+	let size = (grouped.len(), grouped[0].1.len());
+	let (x_size, y_size) = size;
 
 	let total_mines = match matches.value_of("mines") {
 		Some(val) => val.parse().unwrap(),
 		None => {
-			match shape {
+			match size {
 				(8, 8) => 10,
 				(9, 9) => 10,
 				(16, 16) => 40,
 				(30, 16) => 99,
-				_ => panic!("Can't detect number of mines automaticly for shape {:?}. Use (-m, --mines) parameter to specify it.", shape),
+				_ => panic!("Can't detect number of mines automaticly for shape {:?}. Use (-m, --mines) parameter to specify it.", size),
 			}
 		},
 	};
 	let mut mines_left = total_mines;
 
-	let mut grid = Array2::<Square>::from_elem(shape, Square::Unexplored);
+	let mut grid = Array2::<Square>::from_elem(size, Square::Unexplored);
 
 	/*
 	let mut coords: HashMap<Pos, Point> = HashMap::new();
@@ -207,9 +222,9 @@ fn main() {
 		.par_iter()
 		.enumerate()
 		.flat_map(|(x, (_k, g))| g.par_iter().enumerate().map(move |(y, &p)| ((x, y), p)))
-		.collect::<HashMap<Pos, Point>>();
+		.collect::<FxHashMap<Pos, Point>>();
 
-	println!("--- Initial State Success ---");
+	debug!("--- Initial State Success ---");
 	let start = (x_size / 2, y_size / 2);
 
 	let c = coords[&start];
@@ -217,26 +232,26 @@ fn main() {
 		.unwrap_or_else(|_| panic!("Can't move cursor to {:?}", c));
 	click(Button::Left, Some(0));
 
-	let mut solved = HashSet::new();
-	let mut flagged = HashSet::new();
+	let mut solved = FxHashSet::default();
+	let mut flagged = FxHashSet::default();
 
-	let mut to_update = HashSet::new();
-	let mut to_update_reserve = HashSet::new();
+	let mut to_update = FxHashSet::default();
+	let mut to_update_reserve = FxHashSet::default();
 	to_update.insert(start);
 
-	let mut to_solve = HashSet::new();
+	let mut to_solve = FxHashSet::default();
 
 	// Main loop
 	loop {
 		// End game check
 		let smiley = capture_screen_portion(smiley_location).unwrap();
 		if smiley.bitmap_eq(&smiley_win, None) || smiley.bitmap_eq(&smiley_lose, None) {
-			println!("Game finished.");
+			debug!("Game finished.");
 			break;
 		}
 
 		// Updating grid
-		println!("Updating grid...");
+		trace!("Updating grid...");
 		if !to_update.is_empty() {
 			sleep(Duration::from_millis(cooldown));
 			update(
@@ -246,9 +261,9 @@ fn main() {
 				&coords,
 				&solved,
 				&tiles,
+				// &colors,
 				tile_size,
-				x_size,
-				y_size,
+				size,
 			);
 			to_update_reserve = to_update.clone();
 			to_update.clear();
@@ -261,16 +276,16 @@ fn main() {
 				&coords,
 				&solved,
 				&tiles,
+				// &colors,
 				tile_size,
-				x_size,
-				y_size,
+				size,
 			);
 		}
 
 		// Solving state
-		println!("Solving state...");
-		let mut to_open = HashSet::new();
-		let mut to_flag = HashSet::new();
+		trace!("Solving state...");
+		let mut to_open = FxHashSet::default();
+		let mut to_flag = FxHashSet::default();
 
 		// Simple solver
 		to_solve = to_solve
@@ -279,7 +294,7 @@ fn main() {
 			.filter(|&pos| {
 				let mut flagged_mines = 0;
 				let mut unexplored = Vec::with_capacity(8);
-				neighbors(pos, x_size, y_size).into_iter().for_each(|npos| {
+				neighbors(pos, size).into_iter().for_each(|npos| {
 					if flagged.contains(&npos) {
 						flagged_mines += 1;
 					} else if grid[npos] == Square::Unexplored {
@@ -315,7 +330,7 @@ fn main() {
 
 		// End game solver
 		if mines_left == 0 {
-			println!("All mines founded - opening remaining squares...");
+			debug!("All mines founded - opening remaining squares...");
 			iproduct!(0..x_size, 0..y_size).for_each(|p| {
 				if grid[p] == Square::Unexplored && !solved.contains(&p) {
 					to_open.insert(p);
@@ -323,74 +338,119 @@ fn main() {
 			});
 		// Complex solver
 		} else if to_open.is_empty() && to_flag.is_empty() {
-			println!("Calculating probabilities...");
+			trace!("Calculating probabilities...");
 
-			let mut probabilities: HashMap<Pos, f64> = HashMap::new();
+			let mut probabilities = FxHashMap::default();
+
 			// separating to sectors
-			let mut sectors: Vec<HashSet<Detector>> = Vec::new();
+			trace!("separating to sectors");
+			// let mut sectors: Vec<FxHashSet<Detector>> = Vec::new();
+			let mut sectors: Vec<Vec<Detector>> = Vec::new();
+
+			let mut in_sector = FxHashSet::<Pos>::default();
+			let mut s = 0;
+			let get_neighbors = |pos| {
+				let mut flagged_mines = 0;
+				let mut unexplored =
+					HashSet::with_capacity_and_hasher(8, BuildHasherDefault::<FxHasher>::default());
+				let mut detectors =
+					HashSet::with_capacity_and_hasher(8, BuildHasherDefault::<FxHasher>::default());
+				neighbors(pos, size).into_iter().for_each(|npos| {
+					if flagged.contains(&npos) {
+						flagged_mines += 1;
+					} else {
+						match grid[npos] {
+							Square::Unexplored => {
+								unexplored.insert(npos);
+							}
+							Square::Empty => {}
+							_ => {
+								let mut n_flagged_mines = 0;
+								let mut n_unexplored = HashSet::with_capacity_and_hasher(
+									8,
+									BuildHasherDefault::<FxHasher>::default(),
+								);
+								neighbors(npos, size).into_iter().for_each(|n_npos| {
+									if flagged.contains(&n_npos) {
+										n_flagged_mines += 1;
+									} else if grid[n_npos] == Square::Unexplored {
+										n_unexplored.insert(n_npos);
+									}
+								});
+								if !unexplored.is_disjoint(&n_unexplored) {
+									detectors.insert(Detector::new(
+										npos,
+										(grid[npos] - n_flagged_mines) as usize,
+										n_unexplored,
+									));
+								}
+							}
+						}
+					}
+				});
+				(
+					Detector::new(pos, (grid[pos] - flagged_mines) as usize, unexplored),
+					detectors,
+				)
+			};
+
 			to_solve = to_solve
 				.iter()
 				.copied()
 				.filter(|&pos| {
-					let mut flagged_mines = 0;
-					let mut unexplored = HashSet::with_capacity(8);
-					neighbors(pos, x_size, y_size).into_iter().for_each(|npos| {
+					if in_sector.contains(&pos) {
+						return false;
+					}
+
+					let (detector, pos_neighbors) = get_neighbors(pos);
+					/*let mut flagged_mines = 0;
+					let mut unexplored =
+						HashSet::with_capacity_and_hasher(8, BuildHasherDefault::<FxHasher>::default());
+					neighbors(pos, size).into_iter().for_each(|npos| {
 						if flagged.contains(&npos) {
 							flagged_mines += 1;
 						} else if grid[npos] == Square::Unexplored {
 							unexplored.insert(npos);
 						}
-					});
+					});*/
 
-					if !unexplored.is_empty() {
-						let detector =
-							Detector::new(pos, (grid[pos] - flagged_mines) as usize, unexplored.clone());
-						for sector in &mut sectors {
-							for dt in sector.iter().cloned() {
-								if !dt.unexplored.is_disjoint(&unexplored) {
-									sector.insert(detector);
-									return true;
-								}
-							}
+					if pos_neighbors.is_empty() {
+						in_sector.insert(pos);
+						false
+					} else {
+						let mut seed_set = pos_neighbors;
+						seed_set.insert(detector.clone());
+
+						match sectors.get_mut(s) {
+							Some(sector) => sector.push(detector),
+							None => sectors.push(vec![detector]),
 						}
-						let mut new_sector = HashSet::new();
-						new_sector.insert(detector);
-						sectors.push(new_sector);
-						return true;
+
+						while !seed_set.is_empty() {
+							seed_set.clone().into_iter().for_each(|q| {
+								if !in_sector.contains(&q.pos) {
+									let q_neighbors = get_neighbors(q.pos).1;
+
+									if !q_neighbors.is_empty() {
+										q_neighbors.iter().for_each(|n| {
+											seed_set.insert(n.clone());
+										});
+									}
+
+									sectors.get_mut(s).unwrap().push(q.clone());
+									in_sector.insert(q.pos);
+								}
+								seed_set.remove(&q);
+							});
+						}
+						s += 1;
+						true
 					}
-					false
 				})
 				.collect();
-			// union some sectors
-			loop {
-				let mut unioned = sectors
-					.clone()
-					.iter()
-					.combinations(2)
-					.filter_map(|s| {
-						let s1 = s[0];
-						let s2 = s[1];
-						if !s1.is_disjoint(&s2)
-							|| iproduct!(s1, s2).any(|(d1, d2)| d1.unexplored.is_disjoint(&d2.unexplored))
-						{
-							if let Some(i) = sectors.iter().position(|s| s == s1) {
-								sectors.remove(i);
-							}
-							if let Some(i) = sectors.iter().position(|s| s == s2) {
-								sectors.remove(i);
-							}
-							Some(s1.union(&s2).cloned().collect())
-						} else {
-							None
-						}
-					})
-					.collect::<Vec<HashSet<Detector>>>();
-				if unioned.is_empty() {
-					break;
-				}
-				sectors.append(&mut unioned);
-			}
+
 			// creating all possible solutions for each sector
+			trace!("creating all possible solutions for each sector");
 			sectors.iter().for_each(|s| {
 				let s_unexplored = s
 					.par_iter()
@@ -399,6 +459,7 @@ fn main() {
 				let solutions = (1..=min(s_unexplored.len(), mines_left))
 					.into_par_iter()
 					.flat_map(|n| {
+						trace!("iteration: {}", n);
 						s_unexplored
 							.iter()
 							.combinations(n)
@@ -406,7 +467,7 @@ fn main() {
 								s.par_iter().all(|dt| {
 									dt.unexplored
 										.intersection(
-											&sol.par_iter().copied().copied().collect::<HashSet<Pos>>(),
+											&sol.par_iter().copied().copied().collect::<FxHashSet<Pos>>(),
 										)
 										.count() == dt.mines_left
 								})
@@ -418,7 +479,7 @@ fn main() {
 					let mut weights = s_unexplored
 						.par_iter()
 						.map(|&p| (p, 0))
-						.collect::<HashMap<Pos, usize>>();
+						.collect::<FxHashMap<Pos, usize>>();
 					solutions.iter().for_each(|sol| {
 						sol.iter().for_each(|&pos| {
 							*weights.entry(*pos).or_insert(0) += 1;
@@ -450,7 +511,7 @@ fn main() {
 						.unwrap();
 					to_open.insert(choosen);
 					to_update.insert(choosen);
-					println!(
+					debug!(
 						"Opening square with minimum probability of being mine - {:?}: {}.",
 						choosen, probability
 					);
@@ -465,7 +526,7 @@ fn main() {
 						solved.insert(pos);
 						mines_left -= 1;
 					});
-					println!("Opening squares with 0% and flagging with 100% probability.");
+					debug!("Opening squares with 0% and flagging with 100% probability.");
 				}
 			} else if to_open.is_empty() && to_flag.is_empty() && to_update.is_empty() {
 				let mut all_unexplored = Vec::new();
@@ -475,61 +536,69 @@ fn main() {
 					}
 				});
 				if all_unexplored.len() == mines_left {
-					println!("Only mines left - flagging remaining squares...");
+					debug!("Only mines left - flagging remaining squares...");
 					all_unexplored.into_iter().for_each(|p| {
 						to_flag.insert(p);
 						flagged.insert(p);
 						solved.insert(p);
 						mines_left -= 1;
 					});
-				} else {
-					println!("Mission impossible - opening random square.");
-					let choosen = all_unexplored.into_iter().choose(&mut rng).unwrap();
+				} else if !all_unexplored.is_empty() {
+					debug!("Mission impossible - opening random square.");
+					let choosen = *all_unexplored.choose(&mut rng).unwrap();
 					to_open.insert(choosen);
 					to_update.insert(choosen);
 				}
 			}
 		}
 
-		to_open.iter().for_each(|pos| {
-			let c = coords[&pos];
-			move_to(Point::new(c.x + half_tile, c.y + half_tile))
-				.unwrap_or_else(|_| panic!("Can't move cursor to {:?}", c));
-			click(Button::Left, Some(0));
-		});
-		if !no_flags {
-			to_flag.iter().for_each(|pos| {
+		let click_all = |positions: FxHashSet<Pos>, button| {
+			positions.into_iter().for_each(|pos| {
 				let c = coords[&pos];
-				move_to(Point::new(c.x + half_tile, c.y + half_tile))
-					.unwrap_or_else(|_| panic!("Can't move cursor to {:?}", c));
-				click(Button::Right, Some(0));
+
+				let destination = Point::new(c.x + half_tile, c.y + half_tile);
+				if smooth {
+					smooth_move(destination, smooth_duration)
+				} else {
+					move_to(destination)
+				}
+				.unwrap_or_else(|_| panic!("Can't move cursor to {:?}", destination));
+
+				click(button, Some(0));
 			});
+		};
+
+		click_all(to_open, Button::Left);
+		if !no_flags {
+			click_all(to_flag, Button::Right);
 		}
-		println!("Loop success.");
+		trace!("Loop success.");
 	}
 }
 
 #[allow(clippy::too_many_arguments)]
 fn update(
-	positions: &mut HashSet<Pos>,
-	to_solve: &mut HashSet<Pos>,
+	positions: &mut FxHashSet<Pos>,
+	to_solve: &mut FxHashSet<Pos>,
 	grid: &mut Array2<Square>,
-	coords: &HashMap<Pos, Point>,
-	solved: &HashSet<Pos>,
+	coords: &FxHashMap<Pos, Point>,
+	solved: &FxHashSet<Pos>,
 	tiles: &[Bitmap],
+	// colors: &[Rgba<u8>],
 	tile_size: Size,
-	x_size: usize,
-	y_size: usize,
+	size: Pos,
 ) {
 	while !positions.is_empty() {
 		positions.clone().into_iter().for_each(|pos| {
 			if grid[pos] == Square::Unexplored && !solved.contains(&pos) {
 				let tile = capture_screen_portion(Rect::new(coords[&pos], tile_size)).unwrap();
 				for (n, t) in tiles.iter().enumerate() {
+					// for (n, c) in colors.iter().enumerate() {
 					if tile.bitmap_eq(t, None) {
+						// if tile.get_pixel(Point::new(9.0, 7.0)) == *c {
 						grid[pos] = Square::from_usize(n).unwrap();
 						if n == 0 {
-							neighbors(pos, x_size, y_size).into_iter().for_each(|np| {
+							neighbors(pos, size).into_iter().for_each(|np| {
 								positions.insert(np);
 							});
 						} else {
@@ -545,31 +614,15 @@ fn update(
 }
 
 #[inline]
-fn neighbors(pos: Pos, x_size: usize, y_size: usize) -> HashSet<Pos> {
+fn neighbors(pos: Pos, size: Pos) -> HashSet<Pos> {
 	let mut result = HashSet::with_capacity(8);
 
-	let x = pos.0;
-	let y = pos.1;
+	let (x, y) = pos;
+	let (x_size, y_size) = size;
 
-	let x1 = if let Some(r) = x.checked_add(1) {
-		if r < x_size {
-			Some(r)
-		} else {
-			None
-		}
-	} else {
-		None
-	};
+	let x1 = x.checked_add(1).filter(|r| r < &x_size);
 	let x_1 = x.checked_sub(1);
-	let y1 = if let Some(r) = y.checked_add(1) {
-		if r < y_size {
-			Some(r)
-		} else {
-			None
-		}
-	} else {
-		None
-	};
+	let y1 = y.checked_add(1).filter(|r| r < &y_size);
 	let y_1 = y.checked_sub(1);
 
 	if let Some(x1) = x1 {
